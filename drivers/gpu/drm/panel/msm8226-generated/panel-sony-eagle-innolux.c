22fc22d2dda03088d21ad30ae63d744dcc50a9f1
Giovanni Ricca <giovanniricca@proton.me>
2025-05-03 20:28:32 +0200

    WIP load panel
    Change-Id: I91383b41959f3fdb2569156c24e26ffc88f97e97

--- drivers/gpu/drm/panel/msm8226-generated/panel-sony-eagle-innolux.c
+++ drivers/gpu/drm/panel/msm8226-generated/panel-sony-eagle-innolux.c
@@ -355,7 +355,7 @@ static void innolux_remove(struct mipi_dsi_device *dsi)
 }
 
 static const struct of_device_id innolux_of_match[] = {
-	{ .compatible = "innolux" }, // FIXME
+	{ .compatible = "sony,eagle-panel-innolux" },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, innolux_of_match);
@@ -364,7 +364,7 @@ static struct mipi_dsi_driver innolux_driver = {
 	.probe = innolux_probe,
 	.remove = innolux_remove,
 	.driver = {
-		.name = "panel-innolux",
+		.name = "panel-eagle-innolux",
 		.of_match_table = innolux_of_match,
 	},
 };
